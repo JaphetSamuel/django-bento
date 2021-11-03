03e1e78731af09521fa763505bbdec2f8df63177
JaphetSamuel <40962898+JaphetSamuel@users.noreply.github.com>
2021-11-03 12:30:42 +0000

    views

--- bento/urls.py
+++ bento/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from . import views
 
 pref='bento.views'
 urlpatterns = [
-    path(r'^image-upload/$', f'{pref}.image_upload', name='bento-image-import'),
-    path(r'^text-upload/$', f'{pref}.text_upload', name='bento-text-import'),
-    path(r'^text-edit/(?P<name>[\w-]+)/$', f'{pref}.text_edit', name='bento-text-edit'),
-    path(r'^image-edit/(?P<name>[\w-]+)/$', f'{pref}.image_edit', name='bento-image-edit'),
+    path(r'^image-upload/$', views.image_upload, name='bento-image-import'),
+    path(r'^text-upload/$', views.text_upload, name='bento-text-import'),
+    path(r'^text-edit/(?P<name>[\w-]+)/$', views.text_edit, name='bento-text-edit'),
+    path(r'^image-edit/(?P<name>[\w-]+)/$', views.image_edit, name='bento-image-edit'),
 ]
