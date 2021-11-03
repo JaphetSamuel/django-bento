ffb6ce4f41838ad8bddcc7430f0fabe85b07cfc9
JaphetSamuel <40962898+JaphetSamuel@users.noreply.github.com>
2021-11-03 12:23:40 +0000

    fix patterns deprecation

--- bento/urls.py
+++ bento/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, url
+from django.urls import path
 
-
-urlpatterns = patterns('bento.views',
-    url(r'^image-upload/$', 'image_upload', name='bento-image-import'),
-    url(r'^text-upload/$', 'text_upload', name='bento-text-import'),
-    url(r'^text-edit/(?P<name>[\w-]+)/$', 'text_edit', name='bento-text-edit'),
-    url(r'^image-edit/(?P<name>[\w-]+)/$', 'image_edit', name='bento-image-edit'),
-)
+pref='bento.views'
+urlpatterns = [
+    path(r'^image-upload/$', f'{pref}.image_upload', name='bento-image-import'),
+    path(r'^text-upload/$', f'{pref}.text_upload', name='bento-text-import'),
+    path(r'^text-edit/(?P<name>[\w-]+)/$', f'{pref}.text_edit', name='bento-text-edit'),
+    path(r'^image-edit/(?P<name>[\w-]+)/$', f'{pref}.image_edit', name='bento-image-edit'),
+]
