4a51f657a2d49aaa3c1f13aab87d96c59404413c
JaphetSamuel <40962898+JaphetSamuel@users.noreply.github.com>
2021-12-24 17:53:38 +0000

    fix error

--- bento/urls.py
+++ bento/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('image-upload/', views.image_upload, name='bento-image-import'),
     path('text-upload/', views.text_upload, name='bento-text-import'),
     path('text-edit/<str:name>/', views.text_edit, name='bento-text-edit'),
-    path('image-edit/<nstr:ame>/', views.image_edit, name='bento-image-edit'),
+    path('image-edit/<str:ame>/', views.image_edit, name='bento-image-edit'),
 ]
