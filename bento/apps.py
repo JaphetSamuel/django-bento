0423152d1bb859edb6213981675e5dd5f7784fd0
JaphetSamuel <40962898+JaphetSamuel@users.noreply.github.com>
2021-12-24 17:47:42 +0000

    fix regex warning in urls.py fix autofield warning in app.py

---
+++ bento/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class bentoConfig(AppConfig):
+    name='bento'
+    default_auto_field = 'django.db.models.BigAutoField'
--- bento/urls.py
+++ bento/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 
 
 urlpatterns = [
-    path(r'^image-upload/$', views.image_upload, name='bento-image-import'),
-    path(r'^text-upload/$', views.text_upload, name='bento-text-import'),
-    path(r'^text-edit/(?P<name>[\w-]+)/$', views.text_edit, name='bento-text-edit'),
-    path(r'^image-edit/(?P<name>[\w-]+)/$', views.image_edit, name='bento-image-edit'),
+    path('image-upload/', views.image_upload, name='bento-image-import'),
+    path('text-upload/', views.text_upload, name='bento-text-import'),
+    path('text-edit/<str:name>/', views.text_edit, name='bento-text-edit'),
+    path('image-edit/<nstr:ame>/', views.image_edit, name='bento-image-edit'),
 ]
